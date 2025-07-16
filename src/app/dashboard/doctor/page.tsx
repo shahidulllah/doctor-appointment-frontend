433d38fef8535646611b62b5fbc1ad06ba502844
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-16 20:41:22 +0600

    modify dashboard page

--- src/app/dashboard/doctor/page.tsx
+++ src/app/dashboard/doctor/page.tsx
@@ -2,12 +2,18 @@
 import { useSelector } from "react-redux";
 import { useAuth } from "@/hooks/useAuth";
 import { RootState } from "@/redux/store";
+import { Loader2 } from "lucide-react";
 
 export default function DoctorDashboard() {
   useAuth();
   const { user, loading } = useSelector((state: RootState) => state.auth);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <p className="text-center mt-12">
+        <Loader2 />
+      </p>
+    );
   if (!user || user.role !== "doctor") return <p>Access denied</p>;
 
   return (
